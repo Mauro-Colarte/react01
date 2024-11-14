152092fbf6e77164f7cf17dc439d8623e66d23f5
Mauro Colarte <colartemauro02@gmail.com>
2024-11-14 14:17:19 -0300

    color-page

---
+++ react01/src/App.jsx
@@ -0,0 +1,17 @@
+// import './App.css'
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import NavBar from "./components/NavBar/NavBar";
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <br/>
+      <ItemListContainer color="color" greeting="Bienvenidos a nuestra página"/>
+    </>
+  )
+}
+
+export default App
---
+++ react01/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,47 @@
+import CardWidget from "../CartWidget/CardWidget";
+import "./NavBar.css";
+
+export default function NavBar() {
+    return (
+<nav className="navbar navbar-expand-lg bg-body-tertiary navColor">
+    <div className="container-fluid nav">
+        <a className="navbar-brand" href="#">McPizza</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Reserva</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" href="#">Contactanos</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" href="#">Ubicación</a>
+                </li>
+                <li className="nav-item">
+                    <CardWidget amount={2} />
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+)
+}
+
+
+//! DIFF manera de crear un componente. no se puede usar el "export default" con 'Const'
+// const NavBar = () => {
+//     return (
+//         <nav>
+//             <ul>
+//                 <li>HOME</li>
+//                 <li>ABOUT</li>
+//                 <li>CONTACT</li>
+//             </ul>
+//         </nav>
+//     )
+// }
+
+// export default NavBar
